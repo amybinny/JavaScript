41141b2262549bf66944034aa5adca5afaf88c61
amybinny <ambiphi@gmail.com>
2025-06-18 22:10:01 -0400

    add event listener usecase

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -1,3 +1,4 @@
+//function to capture all the input details and display them
 function submitFeedback() {
     const username = document.getElementById('name').value;
     const age = document.getElementById('age').value;
@@ -6,9 +7,12 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
-    
+    const experience = document.getElementById('experinceText').value;
+
+    //display alert message when sibmitted
     alert('Thank you for your valuable feedback')
 
+    //Display user details on webpage
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
     document.getElementById('userEmail').innerHTML = email;
@@ -16,14 +20,19 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperince').innerHTML = experience;
 
+    //display which is hidden is made visible only after the user clicks on the submit button
     document.getElementById('userInfo').style.display = 'block';
 }
 
+//assigning constant to id submitBtn available in HTML
 const submitButton=document.getElementById('submitBtn');
 
+//executing submitFeedback on click of submitButton
 submitButton.onclick = submitFeedback;
 
+//Implement key press (here Enter) to submit feedback
 document.addEventListener('keydown', function(event) {
    if (event.key === 'Enter') {
      submitFeedback();
